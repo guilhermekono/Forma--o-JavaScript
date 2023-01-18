f387d1ff4d7a4bb9f41d9577d0eea92cf4431100
Guilherme Kono <briares65@gmail.com>
2023-01-18 12:46:46 -0300

    testando javascript

--- treino/js/main.js
+++ treino/js/main.js
@@ -50,5 +50,69 @@ function exibeDados(clienteArray){
 exibeDados(clienteArray);
 
 
+var clienteObject = new Object();
+
+clienteObject.nomeCliente = "agnes";
+clienteObject.renda = "70";
+clienteObject.dataNascimento = new Date("2000-01-01");
+clienteObject.ativo = {nome: "agnes2"};
+
+function exibirDadosClienteObjeto(objeto){
+
+    for (const i in objeto) {
+        console.log("Chave: ", i);
+        console.log("Valor: ", objeto[i])
+    }
+
+}
+
+exibirDadosClienteObjeto(clienteObject);
+
+
+
+
+var objetoPessoa = {
+    nome: "Luva de pedreiro",
+    cpf: 020120121,
+    dataNascimento: new Date(1981, 3, 14),
+    ativo: true,
+    "teste prop": "teste",
+    contatos: [12345, 1234, 123432],
+    endereco: {rua: "Rua Tal", numero: 1234, cep: 3323},
+    saudacao: function(){
+        return "Olá, me chamo " + this.nome +"!";
+    }
+}
+
+console.log(objetoPessoa);
+
+console.log("teste", objetoPessoa["teste prop"]);
+
+for (var contato of objetoPessoa.contatos) {
+    console.log("Contato: ", contato);
+    
+}
+
+
+for (var key in objetoPessoa.endereco){
+    console.log(key, objetoPessoa.endereco[key])
+}
+
+
+var obj1 = {matricula: 2}
+var obj2 = obj1;
+
+
+
+
+obj2.matricula = null;
+
+console.log(obj1.matricula)
+
+
+
+
+
+//objeto em 
 
 
