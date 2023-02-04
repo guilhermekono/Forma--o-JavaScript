cd1e58e605d08e49c348a682df556851684237b5
Guilherme Kono <briares65@gmail.com>
2023-02-04 17:39:15 -0300

    SQLs

---
+++ projeto-portfolio/rest-api/banco/script-popular.sql
@@ -0,0 +1,4 @@
+INSERT INTO portfolio(descricao, detalhes)
+VALUES('Desenvolvimento de Websites', 'Tecnologias Javascript, NodeJs, Express e MySQL');
+INSERT INTO portfolio(descricao, detalhes)
+VALUES('Desenvolvimento de APIs', 'Tecnologias Javascript, NodeJs, Express e MySQL');
