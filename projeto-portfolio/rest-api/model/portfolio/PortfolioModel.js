be0380eab84ec9b722ece0470672af48e8d1e957
Guilherme Kono <briares65@gmail.com>
2023-02-05 17:23:05 -0300

    Retornando ID específico ao invés de todos

--- projeto-portfolio/rest-api/model/portfolio/PortfolioModel.js
+++ projeto-portfolio/rest-api/model/portfolio/PortfolioModel.js
@@ -5,5 +5,9 @@ module.exports = class PortfolioModel{
     static getTodos(callback){
         return db.query("SELECT * FROM portfolio", callback);
     }
+
+    static getId(id, callback){
+        return db.query("SELECT * FROM portfolio WHERE id_portfolio = ?", [id], callback) //esse ultimo array é um parâmetro que pode ser passado para esse método no qual as sequencias de intens dentro do array substituem os ? na ordem que aparecem na query
+    }
 };
 
--- projeto-portfolio/rest-api/router/portfolioRouter.js
+++ projeto-portfolio/rest-api/router/portfolioRouter.js
@@ -18,6 +18,24 @@ router.get("/", function(req, res, next){
         res.json(resposta);
 
     })
-})
+});
+
+
+router.get("/:id?", function(req, res, next){
+    PortfolioModel.getId(req.params.id, function(erro, retorno){
+        let resposta = new RespostaClass();
+
+        if(erro){
+            resposta.erro = true;
+            resposta.msg = 'Ocorreu um erro';
+            console.log('erro:', erro);
+        }else{
+            resposta.dados = retorno;
+        }
+
+        res.json(resposta);
+
+    })
+});
 
 module.exports = router;
